55d2aaa7cace06153122c6cdf0c1e1d251e5099e
Sebastian Große <sebastian.grosse@icloud.com>
2013-11-28 09:20:47 +0100

    TODO added

--- src/de/hwr/staffbase2/controller/FrontController.java
+++ src/de/hwr/staffbase2/controller/FrontController.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FrontController
  */
+
+//TODO FrontController Pattern anwenden
 @WebServlet("/FrontController")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
