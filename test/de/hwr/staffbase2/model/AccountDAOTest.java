972afea6bf7695cec28289d9778c15b422f25ae6
Sebastian Große <sebastian.grosse@icloud.com>
2013-12-02 18:45:38 +0100

    Tests

--- test/de/hwr/staffbase2/model/AccountDAOTest.java
+++ test/de/hwr/staffbase2/model/AccountDAOTest.java
@@ -1,22 +1,85 @@
 package de.hwr.staffbase2.model;
 
 import static org.junit.Assert.*;
-import java.util.List;
+
 import org.junit.Test;
 
 public class AccountDAOTest {
-
 	@Test
 	public void testInsert() {
-		AccountDAO accountDAO = AccountDAOFactory.getInstance().getAccountDAO();
+		/*
+		 * Account instanzieren
+		 */
 		Account account = AccountFactory.getInstance().getAccount();
 		account.setManager(true);
-		account.setPassword("test");
-		account.setUsername("sebastian");
-		// TODO Employee setzen zum Test.
+		account.setPassword("passwort");
+		account.setUsername("sebastiangrosse");
+		/*
+		 * Employee instanzieren
+		 */
+		Employee employee = EmployeeFactory.getInstance().getEmployee();
+		account.setEmployee(employee);
+		employee.setAccount(account);
+		employee.setPrename("Sebastian");
+		employee.setSurname("Große");
+		employee.setCity("Berlin");
+		employee.setSalary(3000);
+		employee.setStreet("Müllerstraße");
+		employee.setZipcode(11027);
+		/*
+		 * DAO instanzieren
+		 */
+		AccountDAO accountDAO = AccountDAOFactory.getInstance().getAccountDAO();
 		accountDAO.insert(account);
-		List<Account> accounts = accountDAO.find();
-		System.out.println(accounts.toString());
-		fail();
+		/*
+		 * Vergleich anhand der Id
+		 */
+		assertEquals(accountDAO.find(account.getId()).getId(), account.getId());
+		assertEquals(accountDAO.find(account.getId()).isManager(),
+				account.isManager());
 	}
+
+	@Test
+	public void testUpdate() {
+		Account account = AccountFactory.getInstance().getAccount();
+		account.setManager(true);
+		account.setPassword("passwort");
+		account.setUsername("sebastiangrosse");
+		AccountDAO accountDAO = AccountDAOFactory.getInstance().getAccountDAO();
+		accountDAO.insert(account);
+		account.setUsername("leroyhöbold");
+		account.setManager(false);
+		accountDAO.update(account);
+		assertEquals(accountDAO.find(account.getId()).getUsername(),
+				"leroyhöbold");
+		assertEquals(accountDAO.find(account.getId()).isManager(), false);
+
+		// TODO testen
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFind() {
+		fail("Not yet implemented");
+	}
+
 }
---
+++ test/de/hwr/staffbase2/model/JobDAOTest.java
@@ -0,0 +1,34 @@
+package de.hwr.staffbase2.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JobDAOTest {
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFind() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindLong() {
+		fail("Not yet implemented");
+	}
+
+}
